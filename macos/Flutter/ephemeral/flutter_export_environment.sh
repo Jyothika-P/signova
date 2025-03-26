21008b358bba10d13c23e9d748f98c46a607aa78
JyothikaP <jyotikapbhojja@Jyothikas-MacBook-Air.local>
2025-03-27 00:55:58 +0530

    chore: cleanup

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jyothikap/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jyothikap/Desktop/app development/unmute/unmute_v1"
+export "FLUTTER_APPLICATION_PATH=/Users/jyothikap/Desktop/app development/unmute/signova/signova"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
